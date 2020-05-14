1a2b36f212a3c1ff7a09883188dae60f92a1959b
Didier Margarido <didier.margarido@namecheap.com>
2020-05-14 12:58:36 +0100

    Updated App template

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@
     id="app"
     class="app"
   >
+    <h1>VueJs Sortable</h1>
+    <a
+      href="https://github.com/didiermargarido/vue-sortable"
+      target="_blank"
+    >
+      View on GitHub
+    </a> |
+    <a
+      href="https://github.com/SortableJS/Sortable"
+      target="_blank"
+    >
+      View SortableJs library
+    </a>
     <Sortable />
   </div>
 </template>
